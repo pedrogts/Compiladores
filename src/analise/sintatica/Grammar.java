2d34d2ac6b0776da1ed86dee98bfebc991e62f62
Pedro Gabriel <pedrogtx120@gmail.com>
2025-05-26 23:42:24 -0300

    Versão funcional de verdade

--- src/analise/sintatica/Grammar.java
+++ src/analise/sintatica/Grammar.java
@@ -52,37 +52,7 @@ public class Grammar extends Sintatico {
             return null;
         }
     }
-
-    public NoArvore aritmetica(List<Token> tokens) {
-        curPos = 0;
-        arvore = num(tokens);
-
-        while (curPos < tokens.size() && tokens.get(curPos).tipo != TiposToken.EOF) {
-            TiposToken tokenAtual = tokens.get(curPos).tipo;
-            curPos++;
-            NoArvore noDireito = num(tokens);
-
-            switch (tokenAtual) {
-                case MAIS:
-                    arvore = gerarArvMais(arvore, noDireito);
-                    break;
-                case MENOS:
-                    arvore = gerarArvMenos(arvore, noDireito);
-                    break;
-                case MULTI:
-                    arvore = gerarArvMulti(arvore, noDireito);
-                    break;
-                case BARRA:
-                    arvore = gerarArvDiv(arvore, noDireito);
-                    break;
-                default:
-                    erroSin(tokens.stream().map(t -> t.tipo).toList(), curPos);
-                    break;
-            }
-        }
-
-        return arvore;
-    }
+    
 
     public NoArvore fator(List<Token> tokens) {
         return num(tokens);
